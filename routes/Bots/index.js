567a798e518a90de8563bd6e3cd4726eb07f6010
Oluwatoyosi Oyegoke <oluwatoyosi.oyegoke@gmail.com.com>
2021-03-01 10:56:58 +0100

    remove seed url from swagger

--- routes/Bots/index.js
+++ routes/Bots/index.js
@@ -6,29 +6,6 @@ const botController = require('../../controllers/Bots/index')
 router.post('/interact', botController.interact)
 router.post('/initiate', botController.initiate)
 
-/**
- * @swagger
- * tags:
- *   name: BOT
- *   description: Bot  Routes
- */
-
-
-  /**
-* @swagger
-* /bot/seed:
-*   get:
-*     summary:  Get All responses.
-*     tags: [BOT]
-*     description: This route get all Bot responses.
-*     consumes:
-*       — application/json
-*     responses: 
-*       200:
-*         description: 
-*       400:
-*         description: Bad Request.
-*/
 router.get('/seed', botController.seedData)
 
 module.exports = router
