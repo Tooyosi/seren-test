9882e50627fb9f46516ff8422338d4e2b34a18fd
Oluwatoyosi Oyegoke <oluwatoyosi.oyegoke@gmail.com.com>
2021-03-01 07:56:47 +0100

    add responses route to swagger interface"

--- routes/Api/index.js
+++ routes/Api/index.js
@@ -2,7 +2,29 @@ const express = require('express');
 const router = express.Router({ mergeParams: true });
 const apiController = require('../../controllers/Api/index')
 
+/**
+ * @swagger
+ * tags:
+ *   name: Api
+ *   description: Bot reponse Routes
+ */
 
+
+  /**
+* @swagger
+* /api:
+*   get:
+*     summary:  Get All responses.
+*     tags: [Api]
+*     description: This route get all Bot responses.
+*     consumes:
+*       — application/json
+*     responses: 
+*       200:
+*         description: Receive back Responses   .
+*       400:
+*         description: Bad Request.
+*/
 router.get('/', apiController.getreponses)
 
 module.exports = router
